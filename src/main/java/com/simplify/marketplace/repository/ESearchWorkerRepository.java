d8117d8c546b3a1fa7e7f93edce02283054e7827
chaitanya462 <chaitanyaprasad794@gmail.com>
2021-09-07 14:35:24 +0530

    search apis added to ElasticSearch Repo

--- src/main/java/com/simplify/marketplace/repository/ESearchWorkerRepository.java
+++ src/main/java/com/simplify/marketplace/repository/ESearchWorkerRepository.java
@@ -57,4 +57,321 @@ public interface ESearchWorkerRepository extends ElasticsearchRepository<Elastic
 
     @Query("{\r\n" + "   \"match\":{\r\n" + "     \"jobPreferences.locationType\":\"?0\"\r\n" + "     \r\n" + "   }\r\n" + "  }")
     public ArrayList<ElasticWorker> searchBylocationType(String str);
+
+    @Query(
+        "{\r\n" +
+        "    \"bool\": {\r\n" +
+        "      \"must\": [\r\n" +
+        "        {\r\n" +
+        "          \"match\": {\r\n" +
+        "            \"Category\": \"?0\"\r\n" +
+        "          }\r\n" +
+        "        }\r\n" +
+        "      ]\r\n" +
+        "      , \"should\": [\r\n" +
+        "        {\r\n" +
+        "          \r\n" +
+        "         \"bool\": {\r\n" +
+        "           \"must\": [\r\n" +
+        "             {\r\n" +
+        "               \"match\": {\r\n" +
+        "                 \"jobPreferences.subCategory.name\": \"?1\"\r\n" +
+        "               }\r\n" +
+        "             }\r\n" +
+        "             ,\r\n" +
+        "             {\r\n" +
+        "               \r\n" +
+        "                \"match\": {\r\n" +
+        "                 \"jobPreferences.locationPrefrences.pincode\": \"?2\"\r\n" +
+        "               }\r\n" +
+        "             },\r\n" +
+        "             {\r\n" +
+        "               \"bool\": {\r\n" +
+        "      \"should\": [\r\n" +
+        "        {\r\n" +
+        "          \"multi_match\": {\r\n" +
+        "            \"query\": \"?3\",\r\n" +
+        "            \"type\": \"phrase\",\r\n" +
+        "            \"boost\": 100, \r\n" +
+        "            \"fields\": [\"*\"]\r\n" +
+        "          }}\r\n" +
+        "          ,\r\n" +
+        "          {\r\n" +
+        "          \r\n" +
+        "            \"query_string\": {\r\n" +
+        "            \"query\": \"?3\",\r\n" +
+        "            \"boost\": 10, \r\n" +
+        "            \"default_operator\": \"AND\"\r\n" +
+        "             \r\n" +
+        "          }\r\n" +
+        "            \r\n" +
+        "          \r\n" +
+        "          \r\n" +
+        "            } ,\r\n" +
+        "        \r\n" +
+        "            {\r\n" +
+        "           \r\n" +
+        "          \"query_string\": {\r\n" +
+        "            \"query\": \"?3\",\r\n" +
+        "            \"boost\": 1, \r\n" +
+        "            \"default_operator\": \"OR\"\r\n" +
+        "             \r\n" +
+        "          }\r\n" +
+        "            }\r\n" +
+        "          \r\n" +
+        "          \r\n" +
+        "        \r\n" +
+        "      ]\r\n" +
+        "    }\r\n" +
+        "             }\r\n" +
+        "           ]\r\n" +
+        "         }\r\n" +
+        "          \r\n" +
+        "          \r\n" +
+        "        }\r\n" +
+        "      ]\r\n" +
+        "    }\r\n" +
+        "  }"
+    )
+    public ArrayList<ElasticWorker> allfiltersOr(String Category, String SubCategory, String location, String searchText);
+
+    @Query(
+        "{\r\n" +
+        "   \"bool\": {\r\n" +
+        "     \"must\": [\r\n" +
+        "       {\r\n" +
+        "          \"match\": {\r\n" +
+        "            \"Category\": \"?0\"\r\n" +
+        "          }\r\n" +
+        "       },\r\n" +
+        "       {\r\n" +
+        "         \"match\": {\r\n" +
+        "                 \"jobPreferences.subCategory.name\": \"?1\"\r\n" +
+        "               }\r\n" +
+        "       }\r\n" +
+        "       ,\r\n" +
+        "       {\r\n" +
+        "        \"bool\": {\r\n" +
+        "      \"should\": [\r\n" +
+        "        {\r\n" +
+        "          \"multi_match\": {\r\n" +
+        "            \"query\": \"?2\",\r\n" +
+        "            \"type\": \"phrase\",\r\n" +
+        "            \"boost\": 100, \r\n" +
+        "            \"fields\": [\"*\"]\r\n" +
+        "          }}\r\n" +
+        "          ,\r\n" +
+        "          {\r\n" +
+        "          \r\n" +
+        "            \"query_string\": {\r\n" +
+        "            \"query\": \"?2\",\r\n" +
+        "            \"boost\": 10, \r\n" +
+        "            \"default_operator\": \"AND\"\r\n" +
+        "             \r\n" +
+        "          }\r\n" +
+        "            \r\n" +
+        "          \r\n" +
+        "          \r\n" +
+        "            } ,\r\n" +
+        "        \r\n" +
+        "            {\r\n" +
+        "           \r\n" +
+        "          \"query_string\": {\r\n" +
+        "            \"query\": \"?2\",\r\n" +
+        "            \"boost\": 1, \r\n" +
+        "            \"default_operator\": \"OR\"\r\n" +
+        "             \r\n" +
+        "          }\r\n" +
+        "            }\r\n" +
+        "          \r\n" +
+        "          \r\n" +
+        "        \r\n" +
+        "      ]\r\n" +
+        "    }\r\n" +
+        "       }\r\n" +
+        "       ,\r\n" +
+        "       {\r\n" +
+        "         \"match\": {\r\n" +
+        "                 \"jobPreferences.locationPrefrences.city\": \"?3\"\r\n" +
+        "               }\r\n" +
+        "       }\r\n" +
+        "     ]\r\n" +
+        "   }\r\n" +
+        "  }"
+    )
+    public ArrayList<ElasticWorker> allfilters(String Category, String SubCategory, String location, String searchText);
+
+    @Query(
+        "{\r\n" +
+        "   \"bool\": {\r\n" +
+        "     \"must\": [\r\n" +
+        "       {\r\n" +
+        "          \"match\": {\r\n" +
+        "            \"Category\": \"?0\"\r\n" +
+        "          }\r\n" +
+        "       }\r\n" +
+        "       \r\n" +
+        "       ,\r\n" +
+        "       {\r\n" +
+        "        \"bool\": {\r\n" +
+        "      \"should\": [\r\n" +
+        "        {\r\n" +
+        "          \"multi_match\": {\r\n" +
+        "            \"query\": \"?1\",\r\n" +
+        "            \"type\": \"phrase\",\r\n" +
+        "            \"boost\": 100, \r\n" +
+        "            \"fields\": [\"*\"]\r\n" +
+        "          }}\r\n" +
+        "          ,\r\n" +
+        "          {\r\n" +
+        "          \r\n" +
+        "            \"query_string\": {\r\n" +
+        "            \"query\": \"?1\",\r\n" +
+        "            \"boost\": 10, \r\n" +
+        "            \"default_operator\": \"AND\"\r\n" +
+        "             \r\n" +
+        "          }\r\n" +
+        "            \r\n" +
+        "          \r\n" +
+        "          \r\n" +
+        "            } ,\r\n" +
+        "        \r\n" +
+        "            {\r\n" +
+        "           \r\n" +
+        "          \"query_string\": {\r\n" +
+        "            \"query\": \"?1\",\r\n" +
+        "            \"boost\": 1, \r\n" +
+        "            \"default_operator\": \"OR\"\r\n" +
+        "             \r\n" +
+        "          }\r\n" +
+        "            }\r\n" +
+        "          \r\n" +
+        "          \r\n" +
+        "        \r\n" +
+        "      ]\r\n" +
+        "    }\r\n" +
+        "       }\r\n" +
+        "       \r\n" +
+        "     ]\r\n" +
+        "   }\r\n" +
+        "  }"
+    )
+    public ArrayList<ElasticWorker> searchByCategoryText(String Category, String searchText);
+
+    @Query(
+        "{\r\n" +
+        "   \"bool\": {\r\n" +
+        "     \"must\": [\r\n" +
+        "       {\r\n" +
+        "          \"match\": {\r\n" +
+        "            \"Category\": \"?0\"\r\n" +
+        "          }\r\n" +
+        "       },\r\n" +
+        "       {\r\n" +
+        "         \"match\": {\r\n" +
+        "                 \"jobPreferences.subCategory.name\": \"?1\"\r\n" +
+        "               }\r\n" +
+        "       }\r\n" +
+        "       ,\r\n" +
+        "       {\r\n" +
+        "        \"bool\": {\r\n" +
+        "      \"should\": [\r\n" +
+        "        {\r\n" +
+        "          \"multi_match\": {\r\n" +
+        "            \"query\": \"?2\",\r\n" +
+        "            \"type\": \"phrase\",\r\n" +
+        "            \"boost\": 100, \r\n" +
+        "            \"fields\": [\"*\"]\r\n" +
+        "          }}\r\n" +
+        "          ,\r\n" +
+        "          {\r\n" +
+        "          \r\n" +
+        "            \"query_string\": {\r\n" +
+        "            \"query\": \"?2\",\r\n" +
+        "            \"boost\": 10, \r\n" +
+        "            \"default_operator\": \"AND\"\r\n" +
+        "             \r\n" +
+        "          }\r\n" +
+        "            \r\n" +
+        "          \r\n" +
+        "          \r\n" +
+        "            } ,\r\n" +
+        "        \r\n" +
+        "            {\r\n" +
+        "           \r\n" +
+        "          \"query_string\": {\r\n" +
+        "            \"query\": \"?2\",\r\n" +
+        "            \"boost\": 1, \r\n" +
+        "            \"default_operator\": \"OR\"\r\n" +
+        "             \r\n" +
+        "          }\r\n" +
+        "            }\r\n" +
+        "          \r\n" +
+        "          \r\n" +
+        "        \r\n" +
+        "      ]\r\n" +
+        "    }\r\n" +
+        "       }\r\n" +
+        "       \r\n" +
+        "     ]\r\n" +
+        "   }\r\n" +
+        "  }"
+    )
+    public ArrayList<ElasticWorker> searchByCategorySubText(String Category, String SubCategory, String searchText);
+
+    @Query(
+        "{\r\n" +
+        "   \"bool\": {\r\n" +
+        "     \"must\": [\r\n" +
+        "       \r\n" +
+        "       {\r\n" +
+        "        \"bool\": {\r\n" +
+        "      \"should\": [\r\n" +
+        "        {\r\n" +
+        "          \"multi_match\": {\r\n" +
+        "            \"query\": \"?0\",\r\n" +
+        "            \"type\": \"phrase\",\r\n" +
+        "            \"boost\": 100, \r\n" +
+        "            \"fields\": [\"*\"]\r\n" +
+        "          }}\r\n" +
+        "          ,\r\n" +
+        "          {\r\n" +
+        "          \r\n" +
+        "            \"query_string\": {\r\n" +
+        "            \"query\": \"?0\",\r\n" +
+        "            \"boost\": 10, \r\n" +
+        "            \"default_operator\": \"AND\"\r\n" +
+        "             \r\n" +
+        "          }\r\n" +
+        "            \r\n" +
+        "          \r\n" +
+        "          \r\n" +
+        "            } ,\r\n" +
+        "        \r\n" +
+        "            {\r\n" +
+        "           \r\n" +
+        "          \"query_string\": {\r\n" +
+        "            \"query\": \"?0\",\r\n" +
+        "            \"boost\": 1, \r\n" +
+        "            \"default_operator\": \"OR\"\r\n" +
+        "             \r\n" +
+        "          }\r\n" +
+        "            }\r\n" +
+        "          \r\n" +
+        "          \r\n" +
+        "        \r\n" +
+        "      ]\r\n" +
+        "    }\r\n" +
+        "       }\r\n" +
+        "       ,\r\n" +
+        "       {\r\n" +
+        "         \"match\": {\r\n" +
+        "                 \"jobPreferences.locationPrefrences.city\": \"?1\"\r\n" +
+        "               }\r\n" +
+        "       }\r\n" +
+        "     ]\r\n" +
+        "   }\r\n" +
+        "  }"
+    )
+    public ArrayList<ElasticWorker> searchByLocationText(String searchText, String location);
 }
